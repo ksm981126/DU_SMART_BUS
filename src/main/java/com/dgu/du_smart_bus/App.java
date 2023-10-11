272ad87709ce49572847f7df644dc41c54695b56
김성민 <ksm981126@naver.com>
2023-10-11 21:01:54 +0900

    파이어베이스 자바 연동

--- src/main/java/com/dgu/du_smart_bus/App.java
+++ src/main/java/com/dgu/du_smart_bus/App.java
@@ -3,9 +3,16 @@ package com.dgu.du_smart_bus;
 import java.io.FileInputStream;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ExecutionException;
+import java.util.List;
 
+import com.google.api.core.ApiFuture;
 import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.firestore.DocumentReference;
 import com.google.cloud.firestore.Firestore;
+import com.google.cloud.firestore.QueryDocumentSnapshot;
+import com.google.cloud.firestore.QuerySnapshot;
+import com.google.cloud.firestore.WriteResult;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.cloud.FirestoreClient;
@@ -21,7 +28,7 @@ public class App {
             app.init();
             app.makeDatabaseConn();
             app.select();
-            app.insert();
+            //app.insert();
             //app.update();
             //app.delete();            
         } catch (Exception e) {
@@ -44,33 +51,48 @@ public class App {
         db = FirestoreClient.getFirestore();
     }    
     
-    private void select(){ //조회
-        db.collection(COLLECTION_NAME).addSnapshotListener( (target, exception)->{
-            System.out.println(" - select start - ");
-            target.forEach( item->{
-                System.out.println("primary id : "+item.getId() + "  ||  value : " + item.getData());
-            });
-            System.out.println(" - select end - ");
-        });
+    private void select() throws Exception{ //조회
+        ApiFuture<QuerySnapshot> query = db.collection(COLLECTION_NAME).get();
+    
+        QuerySnapshot querySnapshot = query.get();
+        List<QueryDocumentSnapshot> documents = querySnapshot.getDocuments();
+        for (QueryDocumentSnapshot document : documents) {
+            System.out.println("User: " + document.getId());
+            System.out.println("name: " + document.getString("name"));
+            System.out.println("id: " + document.getLong("numbers"));  
+            System.out.println("id: " + document.getString("id"));
+            System.out.println("booleans: " + document.getBoolean("booleans"));
+        }
     }    
 
     private void insert(){  //등록
-        Map<Object, Object> item = new HashMap<Object, Object>();
-        item.put("name", "HELLO-WORLD5");
-        item.put("numbers", 5674);
-        item.put("booleans", false);
-        db.collection(COLLECTION_NAME).add(item);
+        DocumentReference docRef = db.collection(COLLECTION_NAME).document("test6");
+        Map<Object, Object> data = new HashMap<Object, Object>();
+        data.put("name", "kim");
+        data.put("numbers", "1234");
+        data.put("id", "kk");
+
+    try {
+        ApiFuture<WriteResult> result = docRef.set(data);
+        System.out.println("Update time : " + result.get().getUpdateTime());
+    } catch (InterruptedException e) {
+        // InterruptedException 처리
+        System.err.println("Thread was interrupted while waiting for the result.");
+    } catch (ExecutionException e) {
+        // ExecutionException 처리
+        System.err.println("An error occurred while executing the Firestore operation: " + e.getCause());
+    }
     }
 
-    /*private void update(){  //수정
+    private void update(){  //수정
         Map<String, Object> update = new HashMap<String, Object>();
         update.put("name", "HELLO-WORLD9");
         update.put("numbers", 2341);
         update.put("booleans", false);    	
-        db.collection(COLLECTION_NAME).document("문서 키 값").update(update);
-    }*/
+        db.collection(COLLECTION_NAME).document("test4").update(update);
+    }
 
-    /*private void delete(){  //삭제
-        db.collection(COLLECTION_NAME).document("문서 키 값").delete();
-    } */   
+    private void delete(){  //삭제
+        db.collection(COLLECTION_NAME).document("test1").delete();
+    }  
 }
